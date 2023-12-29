0542a44199fe214fa44660e2923ec91d0ee53ac1
u7chan <t.unagami.dev@gmail.com>
2023-12-30 00:25:44 +0900

    fix: timeline layout

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,3 +1,48 @@
+import {
+  Avatar,
+  Card,
+  Center,
+  Container,
+  HStack,
+  VStack,
+  Wrap,
+  Text,
+} from '@yamada-ui/react'
+
 export default function Timeline() {
-  return <>Timeline</>
+  return (
+    <Container>
+      <Center>
+        <VStack sx={{ width: '100%' }}>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </VStack>
+      </Center>
+    </Container>
+  )
+}
+
+function Post() {
+  const name = 'John Doe'
+  const text =
+    'この文章はダミーです。\n文字の大きさ、量、字間、行間等を確認するために入れています。\nこの文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。'
+  return (
+    <Card sx={{ p: 4 }}>
+      <HStack alignItems='top'>
+        <Wrap gap='md'>
+          <Avatar name={name} />
+        </Wrap>
+        <VStack gap='0'>
+          <Text fontWeight='bold'>{name}</Text>
+          <Text whiteSpace='pre-wrap'>{text}</Text>
+        </VStack>
+      </HStack>
+    </Card>
+  )
 }
