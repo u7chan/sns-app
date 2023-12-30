27fa9b16957b8c4f0c25c3ec4119fbb41fe6b7d9
u7chan <t.unagami.dev@gmail.com>
2023-12-30 10:25:15 +0900

    fix: Post layout

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -29,9 +29,11 @@ export default function Timeline() {
 }
 
 function Post() {
+  const id = 'john_doe'
   const name = 'John Doe'
   const text =
     'この文章はダミーです。\n文字の大きさ、量、字間、行間等を確認するために入れています。\nこの文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。'
+  const createdAt = new Date()
   return (
     <Card sx={{ p: 4 }}>
       <HStack alignItems='top'>
@@ -39,8 +41,16 @@ function Post() {
           <Avatar name={name} />
         </Wrap>
         <VStack gap='0'>
-          <Text fontWeight='bold'>{name}</Text>
+          <HStack>
+            <Text fontWeight='bold'>{name}</Text>
+            <Text fontSize='sm' color='gray.500'>
+              @{id}
+            </Text>
+          </HStack>
           <Text whiteSpace='pre-wrap'>{text}</Text>
+          <Text fontSize='sm' color='gray.500' textAlign='end'>
+            {createdAt.toLocaleString()}
+          </Text>
         </VStack>
       </HStack>
     </Card>
